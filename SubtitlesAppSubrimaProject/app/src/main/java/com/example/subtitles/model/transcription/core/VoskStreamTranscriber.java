83e813f7336b08f4d01d441647f1974cf6d9bca4
Vaibhav-Yadav010 <84952729+Vaibhav-Yadav010@users.noreply.github.com>
2026-09-10 05:17:58 +0530

    Add ArrayBlockingQueue import to VoskStreamTranscriber

--- SubtitlesAppSubrimaProject/app/src/main/java/com/example/subtitles/model/transcription/core/VoskStreamTranscriber.java
+++ SubtitlesAppSubrimaProject/app/src/main/java/com/example/subtitles/model/transcription/core/VoskStreamTranscriber.java
@@ -26,6 +26,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
